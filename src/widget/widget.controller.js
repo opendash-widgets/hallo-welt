83695a2b8e981102b185cb1b6c80263be266ccd1
Sebastian Zilles <dev@zilles.io>
2017-03-22 00:03:28 +0100

    Use od-loading

--- src/widget/widget.controller.js
+++ src/widget/widget.controller.js
@@ -1,10 +1,14 @@
 export default class WidgetController {
 
-  static $inject = ['od.adapter.service', '$element', '$scope', 'moment'];
+  static $inject = ['od.adapter.service', '$element', '$scope', 'moment', '$timeout'];
 
-  constructor($adapter, $element, $scope, moment) {
+  constructor($adapter, $element, $scope, moment, $timeout) {
 
     this.$adapter = $adapter;
     this.ftgroup = 1000;
+
+    console.log(this.loading);
+
+    $timeout(() => { this.loading = false }, 1000);
   }
 }
